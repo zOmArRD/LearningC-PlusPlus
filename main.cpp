97363d1d4f29ca0c85950c0f680c95c0402cd555
zOmArRD <omarsena25@gmail.com>
2022-11-16 18:35:49 -0400

    C++ | User Input Strings #1

--- main.cpp
+++ main.cpp
@@ -6,30 +6,10 @@ using namespace std;
 int main() {
     string firstName;
     string lastName;
-    cout << "Enter your first name: ";
+    cout << "Enter your first name: " << endl;
     cin >> firstName; // get user input from the keyboard
+    cout << "Enter your last name: " << endl;
+    cin >> lastName; // get user input from the keyboard
+    cout << "Your name is: " << firstName << " " << lastName << endl;
     return 0;
-}
-
-void stringLength() {
-    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    cout << "The length of the txt string is: " << txt.length();
-}
-
-void dataTypes() {
-    int age = 16;
-    string name = "Omar ";
-    string last_name = "Alshammari";
-    string full_name = name.append(last_name);
-    char sex = 'M';
-    bool isMarried = false;
-
-    if (age != 16) {
-        isMarried = true;
-    }
-
-    cout << "Name: " << full_name << endl;
-    cout << "Age: " << age << endl;
-    cout << "Sex:" << sex << endl;
-    cout << "Married: " << (isMarried ? "Yes" : "No") << endl;
 }
