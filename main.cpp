659a578cba21c71e55d736a7d3e07abf8a75573e
zOmArRD <omarsena25@gmail.com>
2022-11-22 14:27:36 -0400

    C++ | Using Functions

--- main.cpp
+++ main.cpp
@@ -3,81 +3,25 @@
 
 using namespace std;
 
+struct user {
+    string name, age, country;
+};
+
+user createUser();
+
 int main() {
-    string full_name;
-    int age;
+    user user = createUser();
+    cout << "User created: " << user.name << ", " << user.age << ", " << user.country << endl;
+    return 0;
+}
 
-    cout << "Type your full name: " << endl;
-    getline(cin, full_name);
-
-    cout << "Type your age: " << endl;
-    cin >> age;
-
-    bool has_access = false;
-
-    if (age >= 18 || full_name == "Omar Sena") {
-        has_access = true;
-    }
-
-    if (!has_access) {
-        cout << "Access denied - " << full_name << endl;
-        return 0;
-    } else {
-        cout << "Access granted - " << full_name << endl;
-    }
-
-    cout << "Congratulations! You are now a member of our club!" << endl;
-
-    int day;
-    cout << "What is the current day of the week?" << endl;
-    cin >> day;
-
-    string day_of_week;
-    switch (day) {
-        case 1:
-            day_of_week = "Monday";
-            break;
-        case 2:
-            day_of_week = "Tuesday";
-            break;
-        case 3:
-            day_of_week = "Wednesday";
-            break;
-        case 4:
-            day_of_week = "Thursday";
-            break;
-        case 5:
-            day_of_week = "Friday";
-            break;
-        case 6:
-            day_of_week = "Saturday";
-            break;
-        case 7:
-            day_of_week = "Sunday";
-            break;
-        default:
-            cout << "Invalid day" << endl;
-            return 0;
-    }
-    cout << "Today is " << day_of_week << endl;
-
-    cout << "Looping with for" << endl;
-    for (int i = 0; i < 50; ++i) {
-        cout << i;
-    }
-
-    cout << endl << "Finished" << endl;
-
-    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
-    cout << "Cars: " << endl;
-    for (auto & car : cars) {
-        cout << car << endl;
-    }
-
-    cout << "What is your favorite car?" << endl;
-    string favorite_car;
-    cin >> favorite_car;
-
-    cout << "Your favorite car is: " << favorite_car << endl;
-
+user createUser() {
+    user u;
+    cout << "Enter name: ";
+    cin >> u.name;
+    cout << "Enter age: ";
+    cin >> u.age;
+    cout << "Enter country: ";
+    cin >> u.country;
+    return u;
 }
