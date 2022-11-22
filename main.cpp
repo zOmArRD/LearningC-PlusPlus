d8d0af22cc330fa9af4d07f332e4b8d68e4612e7
zOmArRD <omarsena25@gmail.com>
2022-11-22 14:42:40 -0400

    C++ | Using Functions #2

--- main.cpp
+++ main.cpp
@@ -9,9 +9,40 @@ struct user {
 
 user createUser();
 
+void doOptions(user user) {
+    int options;
+    cout << "what would you like to do?" << endl;
+    cout << "1. Change name" << endl;
+    cout << "2. Change age" << endl;
+    cout << "3. Change country" << endl;
+    cout << "4. Exit" << endl;
+    cin >> options;
+    switch (options) {
+        case 1:
+            cout << "Enter new name: ";
+            cin >> user.name;
+            doOptions(user);
+            break;
+        case 2:
+            cout << "Enter new age: ";
+            cin >> user.age;
+            doOptions(user);
+            break;
+        case 3:
+            cout << "Enter new country: ";
+            cin >> user.country;
+            doOptions(user);
+            break;
+        case 4:
+            cout << "Exiting..." << endl;
+            break;
+    }
+}
+
 int main() {
     user user = createUser();
-    cout << "User created: " << user.name << ", " << user.age << ", " << user.country << endl;
+    cout << "Welcome, " << user.name << endl;
+    doOptions(user);
     return 0;
 }
 
